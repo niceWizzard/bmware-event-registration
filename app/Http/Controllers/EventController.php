dce68f3c23ce684253b4e3e5115d07e754164f36
Richard Manansala <richard232manansala@gmail.com>
2025-07-10 16:29:35 +0800

    Made create event feature work

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 class EventController extends Controller
 {
@@ -21,13 +24,59 @@ class EventController extends Controller
         return view('events.show', compact('event'));
     }
 
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => ['required', 'string', 'max:255'],
+            'partner' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'venue' => ['required', 'string'],
+            'start_date' => ['required', 'date'],
+            'end_date' => ['required', 'date'],
+            'registration_start_date' => ['required', 'date'],
+            'registration_end_date' => ['required', 'date'],
+        ]);
+
+        $validator->after(function ($validator) use ($request) {
+            $start = Carbon::parse($request->start_date);
+            $end = Carbon::parse($request->end_date);
+            $regStart = Carbon::parse($request->registration_start_date);
+            $regEnd = Carbon::parse($request->registration_end_date);
+
+            if ($start->isAfter($end)) {
+                $validator->errors()->add('start_date', 'The start date must not be after the end date.');
+            }
+
+            if ($regStart->isAfter($regEnd)) {
+                $validator->errors()->add('registration_start_date', 'The registration start date must not be after the registration end date.');
+            }
+
+            if ($regEnd->isAfter($start)) {
+                $validator->errors()->add('registration_end_date', 'Registration must end before the event starts.');
+            }
+            if ($start->equalTo($end)) {
+                $validator->errors()->add('end_date', 'The end date must be after the start date.');
+            }
+        });
+
+        $validator->validate();
+
+        $data = $validator->validated();
+
+        $event = Event::create([
+            ...$data,
+            'slug' => Event::generateSlug($data['title']),
+            'start_date' => Carbon::parse($data['start_date']),
+            'end_date' => Carbon::parse($data['end_date']),
+            'registration_start_date' => Carbon::parse($data['registration_start_date']),
+            'registration_end_date' => Carbon::parse($data['registration_end_date']),
+        ]);
+        return Redirect::route('events.show', $event->slug);
+
+    }
+
     public function create()
     {
         return view('events.create');
-    }
-
-    public function store(Request $request)
-    {
-
     }
 }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,7 +1,59 @@
 <x-auth-layout title="Create Event">
-    <section class="container mx-auto p-8">
+    <section class="container mx-auto w-full p-8 flex flex-col items-center relative">
         <h2 class="text-lg font-bold">
             Create an Event
         </h2>
+        <form
+            x-data="{
+                isLoading: false
+            }"
+            class="pt-4 flex flex-col gap-2"
+            method="post"
+            action="{{route('events.store')}}"
+        >
+            @csrf
+            <x-form-input
+                name="title"
+            />
+            <x-form-input
+                name="partner"
+            />
+            <x-form-input
+                name="venue"
+            />
+            <div class="flex gap-2">
+                <x-form-input
+                    name="start_date"
+                    class="datetime-picker"
+                />
+                <x-form-input
+                    name="end_date"
+                    class="datetime-picker"
+                />
+            </div>
+            <div class="flex gap-2">
+                <x-form-input
+                    name="registration_start_date"
+                    class="datetime-picker"
+                />
+                <x-form-input
+                    name="registration_end_date"
+                    class="datetime-picker"
+                />
+            </div>
+            <x-form-input
+                name="description"
+            />
+            <button class="btn primary">
+                Create
+            </button>
+        </form>
     </section>
+    <script defer>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            window.flatpickr(".datetime-picker", {
+                enableTime: true,
+            });
+        })
+    </script>
 </x-auth-layout>
