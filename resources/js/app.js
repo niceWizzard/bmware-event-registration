0e4c5a9e438516c604c3f95e166a7c1fa1b38865
Richard Manansala <richard232manansala@gmail.com>
2025-07-10 16:43:16 +0800

    Installed qrcode

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 import './bootstrap';
 import {Alpine} from "alpinejs";
 import flatpickr from "flatpickr";
+import QRCode from "qrcode";
 
 window.Alpine = Alpine
 window.flatpickr = flatpickr;
+window.QRCode = QRCode;
+
 
 Alpine.start()
