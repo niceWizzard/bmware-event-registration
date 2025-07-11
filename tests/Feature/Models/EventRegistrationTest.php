3b2639e968586aabba783382e4fa5b35c5079a16
Richard Manansala <richard232manansala@gmail.com>
2025-07-11 09:22:25 +0800

    Fixed EventRegistration test not refreshing/seeding db

--- tests/Feature/Models/EventRegistrationTest.php
+++ tests/Feature/Models/EventRegistrationTest.php
@@ -4,10 +4,15 @@ namespace Tests\Feature\Models;
 
 use App\Models\Event;
 use App\Models\EventRegistration;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class EventRegistrationTest extends TestCase
 {
+    use RefreshDatabase;
+
+    protected bool $seed = true;
+
     public function test_event_token_auto_create(): void
     {
         $event = Event::factory()->create();
