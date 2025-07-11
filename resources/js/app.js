e76a8b71fd53e01ccc9eeb9ae5a2af2e03b6c27e
Richard Manansala <richard232manansala@gmail.com>
2025-07-11 16:32:06 +0800

    Installed quilljs

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,11 @@ import './bootstrap';
 import {Alpine} from "alpinejs";
 import flatpickr from "flatpickr";
 import QRCode from "qrcode";
+import Quill from "quill";
 
 window.Alpine = Alpine
 window.flatpickr = flatpickr;
 window.QRCode = QRCode;
-
+window.Quill = Quill;
 
 Alpine.start()
