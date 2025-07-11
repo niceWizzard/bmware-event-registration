591967bb595052e52335bc4cf3e7b0d1781141f6
Richard Manansala <richard232manansala@gmail.com>
2025-07-11 16:32:42 +0800

    Used quilljs to add description instead of text field

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -9,6 +9,7 @@
             }"
             class="pt-4 flex flex-col gap-2"
             method="post"
+            @submit.prevent="isLoading = true; setDescriptionValue(); $el.submit() "
             action="{{route('events.store')}}"
         >
             @csrf
@@ -45,18 +46,63 @@
                     class="datetime-picker"
                 />
             </div>
-            <x-form-input
-                name="description"
-            />
+            <input type="hidden" name="description"/>
+            <div id="editor" class="w-full min-h-48">
+                {!! old('description') !!}
+            </div>
+            @if($errors->has('description'))
+                <p class="text-error">
+                    {{$errors->first('description')}}
+                </p>
+            @endif
             <button class="btn btn-primary">
                 Create
             </button>
         </form>
     </section>
     <script defer>
+        let quill;
+
+        function setDescriptionValue() {
+            const input = document.querySelector('input[name="description"]');
+            const html = quill.root.innerHTML;
+
+            input.value = html === '<p><br></p>' ? '' : html;
+        }
+
         document.addEventListener('DOMContentLoaded', (event) => {
             window.flatpickr(".datetime-picker", {
                 enableTime: true,
+            });
+            quill = new Quill('#editor', {
+                modules: {
+                    toolbar: [
+                        [{header: [1, 2, false]}],
+                        ['bold', 'italic', 'underline'],
+                        [{list: 'ordered'}, {list: 'bullet'}],
+                        ['link']
+                    ],
+                    clipboard: {
+                        matchers: [
+                            ['img', () => {
+                                return new Delta();
+                            }] // Prevent pasted images
+                        ]
+                    }
+                },
+                placeholder: 'Type your text here...',
+                theme: 'snow'
+            });
+
+            quill.root.addEventListener('drop', function (e) {
+                e.preventDefault();
+            });
+
+            quill.root.addEventListener('paste', function (e) {
+                // Optional: prevent all pasted images or files
+                if (e.clipboardData && e.clipboardData.files.length > 0) {
+                    e.preventDefault();
+                }
             });
         })
     </script>
