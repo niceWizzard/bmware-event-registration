9c90b170b9852007245fbcdadbdc77280fb14641
Richard Manansala <richard232manansala@gmail.com>
2025-07-14 09:04:45 +0800

    Added h3,h4

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -85,7 +85,7 @@
             quill = new Quill('#editor', {
                 modules: {
                     toolbar: [
-                        [{header: [1, 2, false]}],
+                        [{header: [1, 2, 3, 4, false]}],
                         ['bold', 'italic', 'underline'],
                         [{list: 'ordered'}, {list: 'bullet'}],
                         ['link']
