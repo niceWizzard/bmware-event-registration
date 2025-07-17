4b2dddf8d03535f939404ca04a4a675aa307a09c
Richard Manansala <richard232manansala@gmail.com>
2025-07-17 15:38:02 +0800

    Fixed events list having 0 registrations even if not

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -13,7 +13,7 @@ class EventController extends Controller
 {
     public function index()
     {
-        $events = Event::all();
+        $events = Event::withCount('registrations')->get();
 
         return view('events.index', compact('events'));
     }
