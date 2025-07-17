7d8d2606891ff3e1fd913e91cc27c548baadb1e9
Richard Manansala <richard232manansala@gmail.com>
2025-07-17 14:44:18 +0800

    Improved the look of dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,35 @@
+@php
+    use \Illuminate\Support\Str;
+@endphp
 <x-auth-layout title="Dashboard">
-    <a href="{{route('events.create')}}">Create</a>
-    Admin
-    <ul class="flex flex-col  gap-2 p-8">
-        @foreach($events as $event)
-            <li class="list-disc">
-                <p>
-                    {{$event->title}}
-                </p>
-                <p>
-                    {{$event->short_name}}
-                </p>
-            </li>
-        @endforeach
-    </ul>
-
+    <section class="p-8">
+        <div class="flex justify-end w-full">
+            <a href="{{route('events.create')}}" class="btn btn-primary">Create Event</a>
+        </div>
+        <div class="flex flex-wrap justify-center  gap-2 p-8">
+            @foreach($events as $event)
+                <div class="card card-border w-md">
+                    <div class="card-body">
+                        <h3 class="card-title">{{Str::limit($event->title, 64)}}</h3>
+                        <p class="text-sm font-light">{{ $event->created_at?->format('D, h:i A') }}</p>
+                        <div class="card-actions justify-end items-center">
+                            <div class="flex-1">
+                                <span class="text-sm font-medium text-gray-700">
+                                    {{ number_format($event->registrations_count ?? 0) }} <span class="text-gray-500">Registrations</span>
+                                </span>
+                            </div>
+                            <a href="{{route('events.manage', $event->short_name)}}" class="btn">
+                                <x-fas-edit class="size-4"/>
+                                Manage
+                            </a>
+                            <a href="{{route('events.show', $event->short_name)}}" class="btn btn-primary">
+                                <x-fas-eye class="size-4"/>
+                                View
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </section>
 </x-auth-layout>
--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ Route::prefix('/admin')
     ->name('admin')
     ->group(function () {
         Route::get('/', static function () {
-            $events = Event::all();
+            $events = Event::withCount('registrations')->get();
 
             return view('admin.dashboard', [
                 'events' => $events,
