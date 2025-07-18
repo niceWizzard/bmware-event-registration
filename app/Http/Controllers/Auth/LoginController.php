f2f7fd772b9fec29564a54ff5537c08979b34b38
Richard Manansala <richard232manansala@gmail.com>
2025-07-18 08:39:09 +0800

    Redirect to dashboard after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/');
+        return redirect()->intended(route('admin.dashboard'));
     }
 
     public function destroy(Request $request)
