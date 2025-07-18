18bc683517dade45519e728462e9a681ebbcee2e
Richard Manansala <richard232manansala@gmail.com>
2025-07-18 14:44:53 +0800

    Added back the imports of prev commented code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Event;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
