fd34ab6de300e91b76058b5405ef5db47d5d5aa2
Richard Manansala <richard232manansala@gmail.com>
2025-08-04 13:19:43 +0800

    Added app name link in card layouts

--- resources/views/layouts/card-layout.blade.php
+++ resources/views/layouts/card-layout.blade.php
@@ -1,14 +1,22 @@
 @props(['class' => ''])
+@php
+    $appName = config('app.name')
+@endphp
 
 <x-base-layout :title="$title" :override-title="$overrideTitle">
-    <main class="min-h-screen bg-surface flex items-center justify-center px-4">
-        <div
-            class="card card-border bg-base-100 md:min-w-96"
-        >
-            <div class="{{twMerge(['card-body', $class])}}">
-                <h2 class="{{twMerge(['card-title', $cardTitleClass])}}">{{$cardTitle}}</h2>
-                {{ $slot }}
+    <div class="flex flex-col gap-2 items-center justify-center min-h-screen">
+        <h3 class="text-3xl text-center font-bold ">
+            <a href="{{ route('home') }}">{{ $appName }}</a>
+        </h3>
+        <main class=" bg-surface flex items-center justify-center px-1">
+            <div
+                class="card card-border bg-base-100 md:min-w-96"
+            >
+                <div class="{{twMerge(['card-body', $class])}}">
+                    <h2 class="{{twMerge(['card-title', $cardTitleClass])}}">{{$cardTitle}}</h2>
+                    {{ $slot }}
+                </div>
             </div>
-        </div>
-    </main>
+        </main>
+    </div>
 </x-base-layout>
